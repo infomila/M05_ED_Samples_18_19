ef3f766e1b72d21463161c5f2e9ac04e8076e29d
Bernat <bernat.orellana@gmail.com>
2018-11-19 15:54:11 +0100

    solució Tarificador teatre

--- DAW/UF2/1_Teatre/src/pkg1_teatre/Tarificador.java
+++ DAW/UF2/1_Teatre/src/pkg1_teatre/Tarificador.java
@@ -13,10 +13,45 @@ public class Tarificador {
     
     public double getPreu( int tipusSeient, int edat ){
         
+        /*
+        Tipus de seient: 
+            1 (platea) : 60€
+            2 (laterals) : 50€
+            3 (primer  pis): 40€
+            4 (segon pis): 30€
+
+        */
+        double[] preusBase = new double[]{60,50,40,30};        
         
-        // codi codi codi
+        if(tipusSeient-1<0||tipusSeient-1>=preusBase.length) {
+            throw new IllegalArgumentException("Tipus de seient fora de rang");
+        }
         
-        return 0;
+        if(edat<0 || edat>=120) {
+            throw new IllegalArgumentException("Edat fora de rang: el rang vàlid és [0,120) ");
+        }
+        
+        //-------------------------------------------------------------------------------------------
+        
+        double preuBase = preusBase[tipusSeient-1];
+        
+        
+        
+        
+        /*
+        Edat del client:
+            <4 anys: no paga
+            [4-12) anys: 25% descompte sobre el seient.
+            [12-60): tarifa normal
+            edat>=60: 50% descompte a platea, la resta al 80% de descompte.
+        */
+        if(edat<4) return 0;
+        if(edat<12) { return 0.75 * preuBase;}
+        if(edat<60) { return preuBase;}
+        else {
+            if(tipusSeient==1) return 0.5*preuBase;
+            else return preuBase - 0.8*preuBase;
+        }        
     }
     
 }
--- DAW/UF2/1_Teatre/test/pkg1_teatre/TarificadorTest.java
+++ DAW/UF2/1_Teatre/test/pkg1_teatre/TarificadorTest.java
@@ -5,10 +5,6 @@
  */
 package pkg1_teatre;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -20,19 +16,68 @@ public class TarificadorTest {
     
     public TarificadorTest() {
     }
-    
 
-    /**
-     * Test of getPreu method, of class Tarificador.
-     */
     @Test
     public void testGetPreu() {
-
-        Tarificador t = new Tarificador();
-        double preu = t.getPreu(1, 2);
-        assertEquals(0, preu,0.001);
+        
+        Tarificador tar = new Tarificador();
+        double preu;
+        double valorEsperat;
+        
+        double preusTest[][] = new double[][]        
+                //platea laterals   primer   segon
+            {
+                {0	,0	    , 0     ,0      }, // <4
+                {45	,37.5       ,30     ,22.5   }, //  4
+                {45	,37.5       ,30     ,22.5   }, //  [4-12)
+                {60	,50	    ,40     ,30     }, //  12
+                {60	,50	    ,40     ,30     }, //  [12-60)
+                {30	,10	    ,8      ,6      }, //  60
+                {30	,10	    ,8      ,6      }  //  >60
+            };
         
         
+        int edatsTest[] = new int[]{2,4, 6,12, 30, 60, 65};
+        //-----------------------------------------------
+        for(int t=0;t<4;t++) {
+            for(int e=0;e<edatsTest.length;e++) {
+                preu = tar.getPreu(t+1, edatsTest[e]);
+                
+                valorEsperat = preusTest[e][t];
+                
+                assertEquals("error amb tipus seient " + (t+1) + 
+                                " i edat "+ edatsTest[e] + 
+                                 " e=" +e , valorEsperat, preu , 0.001);        
+            }
+        }
+        
+        //---------------------------------------------------
+        // fores de rang de seient
+        int tipusSeientErronis[] = new int[]{0,5,-1, 6};
+        for(int t=0;t<tipusSeientErronis.length;t++) {
+            for(int e=0;e<edatsTest.length;e++) {
+                
+                try {
+                    preu = tar.getPreu(tipusSeientErronis[t], edatsTest[e]);
+                    //
+                    fail("No es  gestiona el tipus de seient erroni :"+ tipusSeientErronis[t] );
+                } catch( IllegalArgumentException ex) { /* si estic aquí, tot va bé */}
+            }
+        }
+        //---------------------------------------------------
+        // fores de rang per edat
+        try {
+            preu = tar.getPreu(1, 120);
+            //
+            fail("No es  gestiona el fora de rang per edat (120 anys)");
+        } catch( IllegalArgumentException ex) { /* si estic aquí, tot va bé */}        
+        try {
+            preu = tar.getPreu(1, -1);
+            //
+            fail("No es  gestiona el fora de rang per edat (-1 anys)");
+        } catch( IllegalArgumentException ex) { /* si estic aquí, tot va bé */}        
+        //--------------------------------
+  
     }
     
 }
