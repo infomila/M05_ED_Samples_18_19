ce5f22830d0af9de9bb563a86025f297a8897907
Bernat <bernat.orellana@gmail.com>
2019-03-07 15:37:50 +0100

    Exercici Geometria

---
+++ DAM/UF2/13_Refactoring_3/GeometriaProject/src/mila/Forma.java
@@ -0,0 +1,36 @@
+package mila;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Professor
+ */
+public class Forma {
+
+    public static final int QUADRAT = 1;
+    public static final int CERCLE = 2;
+    public static final int TRIANGE_RECTANGLE_ISOSCELES = 3;
+    public static final int TOTAL_TIPUS_FORMES=3;
+    
+    
+    private int tipusForma;
+    private double mida;
+
+    public Forma(int _tipusForma, double _mida) {
+        this.tipusForma = _tipusForma;
+        this.mida = _mida;
+    }
+
+    public double getMida() {
+        return mida;
+    }
+
+    public int getTipusForma() {
+        return tipusForma;
+    }
+
+}
---
+++ DAM/UF2/13_Refactoring_3/GeometriaProject/test/mila/ColeccioFormesTest.java
@@ -0,0 +1,51 @@
+package mila;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Professor
+ */
+public class ColeccioFormesTest {
+    
+    public ColeccioFormesTest() {
+    }
+
+  
+    /**
+     * Test of informeFormes method, of class ColeccioFormes.
+     */
+    @Test
+    public void testInformeFormes() {
+        ColeccioFormes c = new ColeccioFormes();
+        c.addForma(new Forma(Forma.TRIANGE_RECTANGLE_ISOSCELES, 2));
+        c.addForma(new Forma(Forma.CERCLE, 2));
+        c.addForma(new Forma(Forma.CERCLE, 3));
+        c.addForma(new Forma(Forma.QUADRAT, 3));
+        c.addForma(new Forma(Forma.TRIANGE_RECTANGLE_ISOSCELES, 4));
+        c.addForma(new Forma(Forma.QUADRAT, 3));
+        c.addForma(new Forma(Forma.QUADRAT, 1));
+        String resultat = 
+                "========================"+"\n" +
+                "- Figura <2>: Triange	 mida - 2.0 - area 2"+"\n" +
+                "- Figura <3>: Cercle	 mida - 2.0 - area 12,57"+"\n" +
+                "- Figura <4>: Cercle	 mida - 3.0 - area 28,27"+"\n" +
+                "- Figura <5>: Quadrat	 mida - 3.0 - area 9"+"\n" +
+                "- Figura <6>: Triange	 mida - 4.0 - area 8"+"\n" +
+                "- Figura <7>: Quadrat	 mida - 3.0 - area 9"+"\n" +
+                "- Figura <8>: Quadrat	 mida - 1.0 - area 1"+"\n" +
+                "========================"+"\n" +
+                "Total figures:7, area total:69,84"+"\n" +
+                "========================"+"\n" +
+                "Total Quadrats:3, area final:19"+"\n" +
+                "========================"+"\n" +
+                "Total Cercles:2, area final:40,84"+"\n" +
+                "========================"+"\n" +
+                "Total trianges:2, area final:10"+"\n" +
+                "========================"+"\n";
+        
+        assertTrue( "informe erroni:", resultat.equals(c.informeFormes()));
+    }
+}
